bc586a20bbe30e1ea5280ef7d10aa61d1a1ad661
Harry <harry_bob123@live.co.uk>
2020-04-22 22:59:33 +0100

    Win Screen Added

---
+++ Gameplay Programming Collab/Assets/Scripts/WinScreenManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScreenManager : MonoBehaviour
+{
+    public void BackToStart()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
