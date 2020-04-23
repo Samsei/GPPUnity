29d477565b22703ab64f83ddd403e06ec46e6958
Samsei <samuel3.carr@live.uwe.ac.uk>
2020-04-23 02:05:51 +0100

    Platforms done

---
+++ Gameplay Programming Collab/Assets/moveplat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveplat : MonoBehaviour
+{
+    public Vector3 t1;
+    public Vector3 t2;
+
+    public static float movespeed; //how quickly the platform moves
+    public Vector3 Direction;      //direction of travel
+    public void Start()
+    {
+        movespeed = 4f;
+        Direction = Vector3.right;
+    }
+
+    public void Update()
+    {
+        if (transform.position.z > t2.z || transform.position.z < t1.z)
+            Direction = Direction * -1;
+
+        transform.Translate(Direction * movespeed * Time.deltaTime);
+    }
+}
---
+++ Gameplay Programming Collab/Assets/moveplat2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveplat2 : MonoBehaviour
+{
+    public Vector3 t3;
+    public Vector3 t4;
+
+    public static float movespeed; //how quickly the platform moves
+    public Vector3 Direction1;      //direction of travel
+    public void Start()
+    {
+        movespeed = 4f;
+    }
+
+    public void Update()
+    {
+        if (transform.position.x > t3.x || transform.position.x < t4.x)
+            Direction1 = Direction1 * -1;
+
+        transform.Translate(Direction1 * movespeed * Time.deltaTime);
+    }
+}
